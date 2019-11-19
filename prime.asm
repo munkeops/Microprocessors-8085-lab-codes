34d33a9bb9c6e9efb117c7428e47485c46c1ae3f
rohan9025 <nrogun@gmail.com>
2019-11-19 12:58:30 +0530

    updated code

--- prime.asm
+++ prime.asm
@@ -9,20 +9,25 @@ jmp start
 ;code
 start: nop
 
-lda 1600
+lda 1501h
+mov d,a
+mvi b,02h
+mvi e,00h
+above: call div
+jnz next
+inr e
+next: inr b
+mov a,d
+cmp b
+jnz above
+mov a,e
+sta 1502h
+
 
 hlt
 
-
-
-
-divide: mvi b,02h
-loop: cmp b
-jc next1
-sub b
-inr c
-jmp loop
-next1: sta 1605h
-mov a,c
-sta 1606h
+div: mvi c,00h;beginning of function
+up: sub b
+jnc up
+add b
 ret
